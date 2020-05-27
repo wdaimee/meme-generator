fcb5d691311306940ba4cf145ff1ab824269abed
Waqas D <wdaimee@gmail.com>
2020-05-27 15:33:25 -0400

    Updates

---
+++ precache-manifest.e89b7ecedf307d255d6c57affa5c3d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bf98708a2b1e067b346f51e38f49ce",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "190abb7194d2c5df3cbc",
+    "url": "/meme-generator/static/css/main.ab3ee7f6.chunk.css"
+  },
+  {
+    "revision": "549eba7323bb854d0087",
+    "url": "/meme-generator/static/js/2.69267118.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.69267118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190abb7194d2c5df3cbc",
+    "url": "/meme-generator/static/js/main.b48b0d46.chunk.js"
+  },
+  {
+    "revision": "d61cbcd042e901af7906",
+    "url": "/meme-generator/static/js/runtime-main.df969e4a.js"
+  }
+]);
